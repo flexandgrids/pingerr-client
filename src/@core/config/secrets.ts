c643b28c3ace5e54e99087bf5e2438922048082a
ahmadrazza2001 <mr.ahmedraza7@gmail.com>
2025-02-05 20:42:34 +0500

    ✨ Added: new text formatter and image generation

--- src/@core/config/secrets.ts
+++ src/@core/config/secrets.ts
@@ -1,3 +1,29 @@
+export const openAiKey =
+  "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";
+// const { Configuration, OpenAIApi } = require("openai");
+
+// const config = new Configuration({
+//   apiKey: `${openAiKey}`,
+// });
+
+// const openai = new OpenAIApi(config);
+// const prompt = "";
+// const numberOfImages = 1;
+// const imageSize = "1024x1024";
+
+// openai
+//   .createImage({
+//     prompt: prompt,
+//     n: numberOfImages,
+//     size: imageSize,
+//   })
+//   .then((data: any) => {
+//     console.log(data.data.data);
+//   });
+
+//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
 export const gptKey =
   "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";
 export const geminiKey = "AIzaSyAFqr-Zizrj9X2vQ_dD-HqWDQjKVBdNfnQ";
+export const geminiString =
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=";
